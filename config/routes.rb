494b1e5c1e70ebdd49e2a390c574db80327225e1
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-02-19 11:40:28 -0500

    commit tree 2bc7948ba3166dd82d5d9b8ef0e6af752db34be5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  resources :genres
+  resources :artists
+  resources :songs
+  # get 'song/name'
+
+  # get 'song/artist_id'
+
+  # get 'song/genre_id'
+
+  # get 'artist/name'
+
+  # get 'artist/bio'
+
+  # get 'genre/name'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
